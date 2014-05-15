c145e01d5d3060441a90c430221870316bc25af1
Yossi Rafelson <yossi.rafelson@gmail.com>
2014-05-15 16:11:24 -0700

    big refactor.
    should now verify HMAC signature

--- webhooks.py
+++ webhooks.py
@@ -20,9 +20,9 @@ def getConfig():
         sys.exit('config.json file is not valid json')
 
     for repository in config['repositories']:
-        if(not os.path.isdir(repository['path'])):
+        if not os.path.isdir(repository['path']):
             sys.exit('Directory ' + repository['path'] + ' not found')
-        if(not os.path.isdir(repository['path'] + '/.git')):
+        if not os.path.isdir(repository['path'] + '/.git'):
             sys.exit('Directory ' + repository['path'] + ' is not a Git repository')
 
     return config
@@ -32,52 +32,32 @@ def getPayload(environ):
         length = int(environ.get('CONTENT_LENGTH', '0'))
     except ValueError:
         length = 0
-        
     return environ['wsgi.input'].read(length)
 
 def getUrl(payload):
-    url = json.loads(payload).get('repository', {}).get('url', '')
-    return url
+    return json.loads(payload).get('repository', {}).get('url', '')
 
-def getMatchingPaths(repoUrl):
-    res = []
-    config = getConfig()
-    for repository in config['repositories']:
-        if(repository['url'] == repoUrl):
-            res.append(repository['path'])
-    return res
-
-def pull(path):
-    call(['cd "' + path + '" && git pull'], shell=True)
-
-def deploy(path):
-    config = getConfig()
-    for repository in config['repositories']:
-        if(repository['path'] == path):
-            if 'deploy' in repository:
-                 call(['cd "' + path + '" && ' + repository['deploy']], shell=True)
-            break
-
-def HMAC_OK(payload, hash):
-    key = '123'
-    print payload
+def HMAC_OK(key, payload, hash):
     computed_hash = hmac.new(key, payload, hashlib.sha1).hexdigest()
-    print hash
-    print computed_hash
-    print hash == computed_hash
-    
-    return True
+    return hash.split('=')[-1] == computed_hash
 
 def application(environ, start_response):
     output = 'Go away'
-    #output = pformat(environ)
 
+    config = getConfig()
+    hash = environ.get('HTTP_X_HUB_SIGNATURE', '')
     payload = getPayload(environ)
-    if HMAC_OK(payload, environ.get('HTTP_X_HUB_SIGNATURE', '')):
-        output = 'Thank you, come again!'
-        for path in getMatchingPaths(getUrl(payload)):
-            pull(path)
-            deploy(path)
+    repoUrl = getUrl(payload)
+    
+    for repository in config['repositories']:
+        key = repository.get('key', '')
+        if repository['url'] == repoUrl and HMAC_OK(key, payload, hash):
+            output = 'Thank you, come again!'
+            path = repository['path']
+            deploy = repository.get('deploy', 'touch deploy_time')
+            
+            call(['cd "' + path + '" && git pull'], shell=True) # pull
+            call(['cd "' + path + '" && ' + deploy], shell=True) # deploy
 
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain'),
